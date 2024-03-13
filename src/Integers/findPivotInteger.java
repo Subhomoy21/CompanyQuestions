eb5f1832c197dc9f0b16b7eb1dc7e2d6432833ff
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-13 20:23:14 +0530

    Find the pivot Integer..........

--- src/Integers/findPivotInteger.java
+++ src/Integers/findPivotInteger.java
@@ -1,7 +1,7 @@
 package Integers;
 //Question -> int n = 8
 //            1,2,3,4,5,6,7,8 (1+2+3+4+5+6 = 21) and (6+7+8 = 21)
-//            ans = 6;
+//            ans = 6; TC->O(n^2)
 public class findPivotInteger {
     // naive solution
     static int pivot(int n){
@@ -20,9 +20,23 @@ public class findPivotInteger {
         }
         return -1;
     }
+    // OPTIMISED SOLUTION
+    static int pivot2(int n){
+        int ls= 0;
+        int rs = n*(n+1)/2;
+        for (int i = 1; i <= n; i++) {
+            ls = ls + i;
+            rs = rs - (i-1);
+            if(ls==rs){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int n = 8;
         System.out.println("The pivot Integer is:");
         System.out.println(pivot(n));
+        System.out.println(pivot2(8));
     }
 }
