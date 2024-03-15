3be5e836ea1a8ed2af048fed15ece8ba45ce73b1
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-15 20:27:40 +0530

    Product Of Array Except Self

--- src/Arrays/ProductOfArrayExceptSelf.java
+++ src/Arrays/ProductOfArrayExceptSelf.java
@@ -17,7 +17,7 @@ public class ProductOfArrayExceptSelf {
         return res;
     }
     // using the division process for O(n)
-    static int[] pEs(int[] arr){
+    static int[] pEs(int[] arr){ // yeh error dega arithmetic exception
         int n = arr.length;
         int res [] = new int[n];
         int mul = 1;
@@ -30,6 +30,21 @@ public class ProductOfArrayExceptSelf {
         }
         return res;
     }
+    static int[] productExceptSelf1(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[n];
+        // prefix
+        ans[0] = 1;
+        for(int i = 1;i<n;i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int suffix = 1;
+        for(int j = n-1;j>=0;j--){
+            ans[j] = ans[j] * suffix;
+            suffix = suffix * nums[j];
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int nums[] = {1,2,3,4};
         int [] res = productExceptSelf(nums);
@@ -40,6 +55,11 @@ public class ProductOfArrayExceptSelf {
         int [] res1 = pEs(nums);
         for (int i = 0; i < res1.length; i++) {
             System.out.print(res1[i] + " ");
+        }
+        System.out.println();
+        int [] ans = productExceptSelf1(nums);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
         }
     }
 }
