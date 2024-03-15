d8267ca9c456989a1e543e6e480ce57b349b5bdf
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-15 12:24:04 +0530

    Product Of Array Except Self

--- src/Arrays/ProductOfArrayExceptSelf.java
+++ src/Arrays/ProductOfArrayExceptSelf.java
@@ -16,11 +16,30 @@ public class ProductOfArrayExceptSelf {
         }
         return res;
     }
+    // using the division process for O(n)
+    static int[] pEs(int[] arr){
+        int n = arr.length;
+        int res [] = new int[n];
+        int mul = 1;
+        for (int i = 0; i < n; i++) {
+            mul = mul * arr[i];
+        }
+        for (int i = 0; i < n ; i++) {
+            int num = mul / arr[i];
+            res[i] = num;
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int nums[] = {1,2,3,4};
         int [] res = productExceptSelf(nums);
         for (int i = 0; i < res.length; i++) {
             System.out.print(res[i] + " ");
+        }
+        System.out.println();
+        int [] res1 = pEs(nums);
+        for (int i = 0; i < res1.length; i++) {
+            System.out.print(res1[i] + " ");
         }
     }
 }
