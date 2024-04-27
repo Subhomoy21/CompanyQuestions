e647719e3ab8f0ad7ef2d0bc5df346631c2d1ca7
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-27 13:19:04 +0530

    sub array whose sum is equals k

---
+++ src/Arrays/subarraySumEqualsK.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class subarraySumEqualsK {
+    // naive solution
+    static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            int sum = 0;
+            for(int j = i;j<nums.length;j++){
+                sum = sum + nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+     // effective baki hai!
+    public static void main(String[] args) {
+        int [] arr = {1,1,1};
+        int k = 2;
+        System.out.println(subarraySum(arr,k));
+    }
+}
